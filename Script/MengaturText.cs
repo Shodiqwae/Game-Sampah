1d1e044ba32ef08fb2c1ce46599949456f4f471b
Shadiq <shadiqn07@gmail.com>
2024-02-15 14:50:19 +0700

    Game Sampah

---
+++ Script/MengaturText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MengaturText : MonoBehaviour
+{
+        [Header("Deckripsi Sampah")]
+        public TrackableAR[] tr;
+        [TextArea]
+        public string[] Deskripsi;
+
+        [Header("UI Dekripsi")]
+        public Text txtDescripsi;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i=0; i < tr.Length; i++)
+        {
+            if(tr[i].GetMarker())
+            {
+                txtDescripsi.text = Deskripsi[i];
+            }
+        }
+    }
+}
---
+++ Script/TrackableAR.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackableAR : DefaultTrackableEventHandler
+{
+    private bool marker;
+    public AudioSource suara;
+
+    void awake()
+    {
+        suara = GetComponent<AudioSource>();
+    }
+
+    protected override void OnTrackingFound()
+    {
+        marker = true;
+        base.OnTrackingFound();
+        suara.Play();
+    }
+
+    protected override void OnTrackingLost()
+    {
+        marker = false;
+        base.OnTrackingLost();
+        suara.Stop();
+    }
+
+    public bool GetMarker()
+    {
+        return marker;
+    }
+}
